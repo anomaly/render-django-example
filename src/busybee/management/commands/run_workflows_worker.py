42a85b2abd4b09fe9b2582fabe16d49832939f70
Dev Mukherjee <dev@anomaly.net.au>
2026-06-17 09:33:01 +1000

    chore: ports workflow management command
    - allows user to be able to run the worker via a django management cmd
    - breaking for now as we have to port the settings folder

---
+++ src/busybee/management/commands/run_workflows_worker.py
@@ -0,0 +1,13 @@
+"""
+Django wrapper for render workflow
+"""
+
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+app = settings.WORKFLOW_APP
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        app.start()
